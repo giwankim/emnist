d4b79acb3a541f5d8668187d3d8d290ebfc29668
Giwan <giwan.kim@gmail.com>
2020-08-17 16:44:56 +0900

    Add training parameters to config.py

--- src/config.py
+++ src/config.py
@@ -1,18 +1,20 @@
 import string
 
 
-# Data
+# Data paths
 DATA_PATH = "../input/"
 TRAIN_CSV = DATA_PATH + "train.csv"
 TEST_CSV = DATA_PATH + "test.csv"
 OUTPUT_PATH = "../output/"
+
+# Image stats
 SIZE = 28
-PIXEL_COLS = [str(i) for i in range(SIZE * SIZE)]
-
-LETTER_TO_INDEX = {c: i for i, c in enumerate(string.ascii_uppercase)}
-
 MEAN = 0.143
-STD = 0.253
+STD = 0.254
+
+# Dataframe
+PIXEL_COLS = [str(i) for i in range(SIZE * SIZE)]
+LETTER_TO_INDEX = {c: i for i, c in enumerate(string.ascii_uppercase)}
 
 # Train
 DEVICE = "cuda"
@@ -20,3 +22,4 @@ TRAIN_BATCH_SIZE = 64
 TEST_BATCH_SIZE = 128
 EPOCHS = 150
 PATIENCE = 10
+CLIP_GRAD = 1.0
