24990439b4ab0de7ecaa61a946a2cf46745048de
Giwan <giwan.kim@gmail.com>
2020-08-18 12:55:06 +0900

    Modify model_dispatcher for kfold traning

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,6 +1,10 @@
 import models
 
-MODEL_DISPATCHER = {
-    "baseline": models.Model(),
-    "spinalvgg": models.SpinalVGG(),
-}
+
+def get_model(model_name):
+    if model_name == "baseline":
+        return models.Model()
+    elif model_name == "spinalvgg":
+        return models.SpinalVGG()
+    else:
+        raise RuntimeError("Not a valid model name!")
