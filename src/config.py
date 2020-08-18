a5a7d2ffe28ac20e98cff486cc4205853da35bd5
Giwan <giwan.kim@gmail.com>
2020-08-18 12:56:57 +0900

    Add hyperparameters to config.py

--- src/config.py
+++ src/config.py
@@ -1,25 +1,38 @@
-import string
+import pathlib
 
+# VERSIONING
+MAJOR = 1
+MINOR = 0
 
-# Data paths
-DATA_PATH = "../input/"
-TRAIN_CSV = DATA_PATH + "train.csv"
-TEST_CSV = DATA_PATH + "test.csv"
-OUTPUT_PATH = "../output/"
+DEVICE = "cuda"
+SEED = 42
 
-# Image stats
+# PATHS for data files
+DATA_PATH = pathlib.Path("/home/isleof/Development/emnist/input")
+OUTPUT_PATH = pathlib.Path("/home/isleof/Development/emnist/output")
+MODEL_PATH = pathlib.Path("/home/isleof/Development/emnist/models")
+TRAIN_CSV = DATA_PATH / "train.csv"
+TEST_CSV = DATA_PATH / "test.csv"
+
+# IMAGE stats
 SIZE = 28
 MEAN = 0.143
 STD = 0.254
 
-# Dataframe
-PIXEL_COLS = [str(i) for i in range(SIZE * SIZE)]
-LETTER_TO_INDEX = {c: i for i, c in enumerate(string.ascii_uppercase)}
+# DATAFRAME
+PIXEL_COLS = [str(i) for i in range(784)]
+LETTER_TO_INDEX = {c: i for i, c in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
+NUM_CLASSES = 10
 
-# Train
-DEVICE = "cuda"
+#### Train/Hyperparameters ####
+
+# Batch Sizes
 TRAIN_BATCH_SIZE = 64
 TEST_BATCH_SIZE = 128
-EPOCHS = 150
-PATIENCE = 10
+
+NUM_FOLDS = 5
+EPOCHS = 200
+
 CLIP_GRAD = 1.0
+LR_REDUCE_PATIENCE = 10
+EARLY_STOP_PATIENCE = 15
