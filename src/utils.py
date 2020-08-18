ae84f4eadf08b7ca8eef62ed34944b4dbae10c03
Giwan <giwan.kim@gmail.com>
2020-08-18 22:01:49 +0900

    Modify seed function to include cuda

--- src/utils.py
+++ src/utils.py
@@ -7,5 +7,8 @@ import torch
 def seed_everything(seed):
     random.seed(seed)
     np.random.seed(seed)
-    torch.manual_seed(seed)
     tf.random.set_seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
