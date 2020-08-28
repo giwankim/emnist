3ee62ba99e464afa55b043c8ba792f24e6edfe2d
Giwan <giwan.kim@gmail.com>
2020-08-28 11:10:38 +0900

    Add AverageMeter

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import random
 import numpy as np
 import tensorflow as tf
 import torch
+import torch.nn as nn
+import torch.nn.init as init
 
 
 def seed_everything(seed):
@@ -34,3 +36,30 @@ def init_params(net):
             init.normal(m.weight, std=1e-3)
             if m.bias:
                 init.constant(m.bias, 0)
+
+
+def save_checkpoint(
+    state, is_best, checkpoint="checkpoint", filename="checkpoint.pth.tar"
+):
+    pass
+
+
+class AverageMeter:
+    """Computes and stores the average and current value
+       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum = val * n
+        self.count += n
+        self.avg = self.sum / self.count
