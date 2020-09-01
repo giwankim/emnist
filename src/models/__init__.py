0e730256a91ea0ebd700b16be093bae5475ad468
Giwan <giwan.kim@gmail.com>
2020-09-01 20:53:36 +0900

    Add model and optimizer dispatcher to respective __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -13,3 +13,41 @@ from models.efficientnet import (
     EfficientNetL2,
 )
 from models.resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+
+def get_model(model_name):
+    if model_name == "baseline":
+        return Baseline()
+    elif model_name == "spinalvgg":
+        return SpinalVGG()
+    elif model_name == "efficientnet-b0":
+        return EfficientNetB0()
+    elif model_name == "efficientnet-b1":
+        return EfficientNetB1()
+    elif model_name == "efficientnet-b2":
+        return EfficientNetB2()
+    elif model_name == "efficientnet-b3":
+        return EfficientNetB3()
+    elif model_name == "efficientnet-b4":
+        return EfficientNetB4()
+    elif model_name == "efficientnet-b5":
+        return EfficientNetB5()
+    elif model_name == "efficientnet-b6":
+        return EfficientNetB6()
+    elif model_name == "efficientnet-b7":
+        return EfficientNetB7()
+    elif model_name == "efficientnet-b8":
+        return EfficientNetB8()
+    elif model_name == "efficientnet-l2":
+        return EfficientNetL2()
+    elif model_name == "resnet18":
+        return ResNet18()
+    elif model_name == "resnet34":
+        return ResNet34()
+    elif model_name == "resnet50":
+        return ResNet50()
+    elif model_name == "resnet101":
+        return ResNet101()
+    elif model_name == "resnet152":
+        return ResNet152()
+    else:
+        raise RuntimeError(f"Unknown model name: {model_name}")
--- src/optimizers/__init__.py
+++ src/optimizers/__init__.py
@@ -1,1 +1,34 @@
+import torch
+import adamp
+
 from .radam import RAdam, RAdam_4step
+
+
+def get_optimizer(optim_name, model, lr, weight_decay=1e-4, skip_list=["bias", "bn"]):
+    params = []
+    exclude_params = []
+    for name, param in model.named_parameters():
+        if not param.requires_grad: continue
+        elif any(skip in name for skip in skip_list):
+            exclude_params.append(param)
+        else:
+            params.append(param)
+    optim_params = [
+        {"params": params, "weight_decay": weight_decay},
+        {"params": exclude_params, "weight_decay": 0.0},
+    ]
+    
+    if optim_name == "adam":
+        optimizer = torch.optim.Adam(optim_params, lr=lr, betas=(0.9, 0.999))
+    elif optim_name == "radam":
+        optimizer = RAdam(optim_params, lr=lr, betas=(0.9, 0.999))
+    elif optim_name == "radam4s":
+        optimizer = RAdam_4step(optim_params, lr=lr, betas=(0.9, 0.999))
+    elif optim_name == "adamp":
+        optimizer = adamp.AdamP(optim_params, lr=lr, betas=(0.9, 0.999))
+    elif optim_name == "sgdp":
+        optimizer = adamp.SGDP(optim_params, lr=lr, momentum=0.9, nesterov=True)
+    else:
+        raise RuntimeError(f"Unknown optimizer name: {optim_name}")
+        
+    return optimizer
