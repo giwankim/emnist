8309a1a9fc471092a1f696efc303f540cd68a3c4
Giwan <giwan.kim@gmail.com>
2020-09-14 12:50:07 +0900

    Add ResNet implementation from scratch

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -1,42 +1,173 @@
+import torch
 import torch.nn as nn
-import pretrainedmodels
+import torch.nn.functional as F
+
+import utils
 
 
-def ResNet18(pretrained=None):
-    model = pretrainedmodels.__dict__["resnet18"](pretrained=pretrained)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-    in_feats = model.last_linear.in_features
-    model.last_linear = nn.Linear(in_feats, 10)
-    return model
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(
+        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+    )
 
 
-def ResNet34(pretrained=None):
-    model = pretrainedmodels.__dict__["resnet34"](pretrained=pretrained)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-    in_feats = model.last_linear.in_features
-    model.last_linear = nn.Linear(in_feats, 10)
-    return model
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(self.expansion * planes),
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
-def ResNet50(pretrained=None):
-    model = pretrainedmodels.__dict__["resnet50"](pretrained=pretrained)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-    in_feats = model.last_linear.in_features
-    model.last_linear = nn.Linear(in_feats, 10)
-    return model
+class PreActBlock(nn.Module):
+    """Pre-activation version of the BasicBlock"""
+
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
+
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                )
+            )
+        else:
+            self.shortcut = nn.Sequential()
+
+    def forward(self, x):
+        x = F.relu(self.bn1(x))
+        shortcut = self.shortcut(x)
+        x = self.conv1(x)
+        out = self.conv2(F.relu(self.bn2(x)))
+        out += shortcut
+        return out
 
 
-def ResNet101(pretrained=None):
-    model = pretrainedmodels.__dict__["resnet101"](pretrained=pretrained)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-    in_feats = model.last_linear.in_features
-    model.last_linear = nn.Linear(in_feats, 10)
-    return model
+class Bottleneck(nn.Module):
+    """Pre-activation version of the original Bottleneck module."""
+
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(
+            planes, self.expansion * planes, kernel_size=1, bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(self.expansion * planes),
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
-def ResNet152(pretrained=None):
-    model = pretrainedmodels.__dict__["resnet152"](pretrained=pretrained)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-    in_feats = model.last_linear.in_features
-    model.last_linear = nn.Linear(in_feats, 10)
-    return model
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = conv3x3(1, 64)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(1024 * block.expansion, num_classes)
+        utils.init_cnn(self)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        avg_feats = F.adaptive_avg_pool2d(x, output_size=1)
+        max_feats = F.adaptive_max_pool2d(x, output_size=1)
+        x = torch.cat([avg_feats, max_feats], dim=1)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
+
+
+def ResNet18():
+    return ResNet(PreActBlock, [2, 2, 2, 2])
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def ResNet101():
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
+
+def ResNet152():
+    return ResNet(Bottleneck, [3, 8, 36, 3])
